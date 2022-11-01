1e6654be21ebbd7b046333c005fe08c71a9f77e2
Carzvermax <carzvermax@gmail.com>
2022-11-01 14:37:30 +0200

    HW1

---
+++ HW1/main.js
@@ -0,0 +1,46 @@
+let hello = 'hello';
+console.log (hello);
+let owu = 'owu';
+console.log (owu);
+let com = 'com';
+console.log (com);
+let ua = 'ua';
+console.log (ua);
+let num1 = 1;
+console.log (num1);
+let num2 = 10;
+console.log (num2);
+let num3 = -999;
+console.log (num3);
+let num4 = 123;
+console.log (num4);
+let pi = 3.14;
+console.log (pi);
+let num5 = 2.7;
+console.log (num5);
+let num6 = 16;
+console.log (num6);
+let bool1 = true;
+console.log (bool1);
+let bool2 = false;
+console.log (bool2);
+
+let firstName = 'Maksim';
+let middleName = 'Evgenievich';
+let lastName = 'Shmorgunov';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log (typeof a);
+console.log (typeof b);
+console.log (typeof c);
+
+let name = prompt("Name");
+let patronymic = prompt("patronymic");
+let age = prompt("age");
+console.log(name + ' ' + patronymic + ' ' + age);
+
+

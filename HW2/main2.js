edbef6c73c68bfba22dfa81f32dfb549702c40c3
Carzvermax <carzvermax@gmail.com>
2022-11-03 16:11:14 +0200

    HW2

---
+++ HW2/main2.js
@@ -0,0 +1,86 @@
+let arr = [25, 'ponchik', true, [28, 36, 56], {name: 'Max', Id: 1}, 'reolad', 45, false, 99, 1005000];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+let book1 = {
+    title: 'In Search of Lost Time',
+    pageCount: 537,
+    genre: 'Modernist'
+}
+let book2 = {
+    title: 'Ulysses',
+    pageCount: 732,
+    genre: 'Modernist novel'
+}
+let book3 = {
+    title: 'Don Quixote',
+    pageCount: 1077,
+    genre: 'Spanish epic novel'
+}
+let authors = [{
+    name: 'Marcel Proust',
+    age: 51
+},
+    {
+        name: 'James Joyce',
+        age: 58
+    },
+    {
+        name: 'Miguel de Cervantes',
+        age: 68
+    }]
+book1.author = authors[0]
+book2.author = authors[1]
+book3.author = authors[2]
+console.log(book1)
+let users = [
+    {
+        name: 'Vasya',
+        username: 'Vasya3000',
+        password: 'pupkin2012'},
+    {name: 'Nikola',
+        username: 'Tesla228',
+        password: 'inventor'},
+    {name: 'Ilon',
+        username: 'SpaceX',
+        password: '123pc4560'},
+    {name: 'Katya',
+        username: 'ProGamer3000',
+        password: 'ekaterina050'},
+    {name: 'Greg',
+        username: 'Greg_Pupkin',
+        password: 'samsa20r'},
+    {
+        name: 'Andrey',
+        username: 'Andrey_Rivera',
+        password: 'andeijan1999'},
+    {name: 'Nikita',
+        username: 'ProDota2Gamer',
+        password: 'Dota2theBest'},
+    {name: 'Alina',
+        username: 'Alina_Webster',
+        password: 'nonRPgirl'},
+    {name: 'Vitaliy',
+        username: 'Vitaliy_Pavlov',
+        password: 'adnimTeam2022'},
+    {name: 'Kiril',
+        username: 'Kai_Lemair',
+        password: 'DirectorNuboRP'}
+]
+console.log(users[0].password)
+console.log(users[1].password)
+console.log(users[2].password)
+console.log(users[3].password)
+console.log(users[4].password)
+console.log(users[5].password)
+console.log(users[6].password)
+console.log(users[7].password)
+console.log(users[8].password)
+console.log(users[9].password)
